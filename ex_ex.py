fab510e68106d28ce90f7fdd6f88442b26949b74
Anry <54634437+AnryChe@users.noreply.github.com>
2023-08-02 12:56:54 +0500

    Add files via upload
    Version 1.2 changes:
    - variables are calculated by calling functions;
    - preparation of the RFI response template has been moved to a separate function;
    - Requests with incorrect discipline are sent to private messages for processing;
    - Added the ability to update the RFI list without restarting the program;
    - Answer templates by disciplines are sent to separate Telegram groups by disciplines

--- ex_ex.py
+++ ex_ex.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import os
+import config
+
 
 class InspectionsData:
     def __init__(self, ins_pos, insp_df):
@@ -11,3 +15,26 @@ class InspectionsData:
         self.fndt_smr = self.insp_smr.find("+")
         self.fndt_kiok = self.insp_kiok.find("+")
         self.insp_discipline = insp_df.iloc[ins_pos, 5]
+
+
+def refresh_inspections_file():  # Функция получения дата фрейма с инспекциями.
+    content = os.listdir(config.path)
+    dir_files = []
+    for file in content:  # В цикле планируется сортировать файлы по дате, дабы открывать последний
+        if os.path.isfile(os.path.join(config.path, file)) and file.endswith('.xlsx'):
+            dir_files.append(file)
+    inspections_file = pd.read_excel(dir_files[0], header=4)
+    gen_in_df = inspections_file.iloc[:, 0:11]
+    return gen_in_df
+
+
+def get_discipline_types():  # Функция получения списка дисциплин, по которым есть инспекции
+    ins_df = refresh_inspections_file()
+    disc_types = ins_df['Дисциплина'].str.lower().unique()
+    return disc_types
+
+
+def get_df_len():  # Функция получения длины дата фрейма для определения количества инспекций
+    data_fr = refresh_inspections_file()
+    dframe_len = len(data_fr)
+    return dframe_len
